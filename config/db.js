30c6215bc3f36fd6784f23895e8f49ff85752d5c
teja polnati <tejapolnati5769@gmail.com>
2026-07-30 17:07:42 +0530

    Add database pool test

--- config/db.js
+++ config/db.js
@@ -11,7 +11,31 @@ const db = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0
 });
+const mysql = require("mysql2");
 
+const db = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.log("POOL ERROR:", err);
+    return;
+  }
+
+  console.log("DATABASE CONNECTED");
+
+  connection.release();
+});
+
+module.exports = db;
 console.log("MySQL Pool Created");
 
 module.exports = db;
